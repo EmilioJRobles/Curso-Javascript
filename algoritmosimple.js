7406226d748278fca2cc5c63ff0c4af24878ac97
EmilioR <emiliojrobleslopez@gmail.com>
2021-11-22 22:45:00 -0300

    Desafio Uno

---
+++ algoritmosimple.js
@@ -0,0 +1,28 @@
+//Uso del prompt solicitud de datos al usuario.
+
+let nombre = prompt(' Ingrese su Nombre ');
+let apellido = prompt(' Ingrese su Apellido ');
+let salida = "Bienvenido / Bienvenida : " + nombre + " " + apellido;
+alert(salida);
+console.log(salida);
+
+let rutDni = prompt(" Ingresa tu Rut o Dni   'Opcional'");
+console.log("Número de Identificación : " + rutDni);
+
+let mailUsuario = prompt(" Ingrese su Mail.");
+console.log("Email : " + mailUsuario);
+
+//
+
+let edad = prompt( " Ingrese su edad ");
+presente = parseInt( 2021 );
+let resta = presente - edad;
+console.log ("Su año de nacimiento es : " + resta)
+
+alert("¡Su ingreso fue correcto!");
+
+
+
+
+
+
